added function to compute the variance of the precision matrix + bug fixes

wishart_analysis.py
@@ -265,7 +265,6 @@ def cov_variance(powertype = "power", mainpath = "", noutput = 1 , aexp = 1., gr
 def cov_variance_kcut(kmin=0.03, kmax = 1., powertype = "power", mainpath = "", noutput = 1 , aexp = 1., growth_a = np.zeros(0), growth_dplus = np.zeros(0), list_nr = [10,50,100,500,700,1000,3000,5000,6000], okprint=False):
     
     simset = "all_256"
-    trace_sigma2=np.zeros(len(list_nr))
     i=0 #index on list_nr
     
     power_k,dummy = power_spectrum(powertype,mainpath,"4096_adaphase_256",1,noutput,aexp,growth_a,growth_dplus)
@@ -330,7 +329,7 @@ def cov_variance_kcut(kmin=0.03, kmax = 1., powertype = "power", mainpath = "", 
 
 
 
-# --------------------------- PRECISION MATRIX VARIANCE --------------------------- #
+# --------------------------- PRECISION MATRIX VARIANCE TRACE --------------------------- #
 def inv_cov_variance_trace(powertype = "power", mainpath = "", noutput = 1 , aexp = 1., growth_a = np.zeros(0), growth_dplus = np.zeros(0), list_nr = [300,500,700,1000,3000,5000,6000], okprint=False):
 
     simset = "all_256"
@@ -389,7 +388,7 @@ def inv_cov_variance_trace(powertype = "power", mainpath = "", noutput = 1 , aex
 
 
 
-# --------------------------- PRECISION MATRIX VARIANCE --------------------------- #
+# --------------------------- PRECISION MATRIX VARIANCE TRACE WITH k CUT --------------------------- #
 def inv_cov_variance_trace_kcut(kmin = 0.03, kmax = 1., powertype = "power", mainpath = "", noutput = 1 , aexp = 1., growth_a = np.zeros(0), growth_dplus = np.zeros(0), list_nr = [300,500,700,1000,3000,5000,6000], okprint=False):
     
     simset = "all_256"
@@ -453,6 +452,65 @@ def inv_cov_variance_trace_kcut(kmin = 0.03, kmax = 1., powertype = "power", mai
 
         i+=1
     return list_nr, trace_sigma2
+# ---------------------------------------------------------------------------- #
+
+
+
+# --------------------------- PRECISION VARIANCE --------------------------- #
+def inv_cov_variance(powertype = "power", mainpath = "", noutput = 1 , aexp = 1., growth_a = np.zeros(0), growth_dplus = np.zeros(0), list_nr = [10,50,100,500,700,1000,3000,5000,6000], okprint=False):
+    
+    simset = "all_256"
+    i=0 #index on list_nr
+    
+    power_k,dummy = power_spectrum(powertype,mainpath,"4096_adaphase_256",1,noutput,aexp,growth_a,growth_dplus)
+    nbin = power_k.size
+    
+    sigma2_all = np.zeros((len(list_nr),nbin,nbin))
+    
+    for nr in list_nr:
+        
+        nsub = 12288/nr
+        inv_cov_sub = np.zeros((nsub,nbin,nbin))
+        totsim = 12288 - int(math.fmod(12288,nr))
+        
+        print nr,totsim
+        
+        for isub in xrange(0, nsub):
+            isimmin = isub * nr + 1
+            isimmax = (isub+1)*nr
+            filename="tmp/"+str("%05d"%noutput)+"/cov_"+powertype+"_"+str("%05d"%nr)+"_"+str("%05d"%isub)+".txt"
+            if(os.path.isfile(filename)):
+                power_pcov=np.loadtxt(filename, unpack=True)
+            else:
+                dummy,dummy,dummy,power_pcov=cov_power(powertype, mainpath, simset, isimmin, isimmax+1, noutput, aexp, growth_a, growth_dplus)
+                fltformat="%-.12e"
+                f = open(filename, "w")
+                for ik in xrange(0, nbin):
+                    for jk in xrange(0, nbin):
+                        f.write(str(fltformat%power_pcov[ik,jk])+" ")
+                    f.write("\n")
+                f.close()
+            inv_cov_sub[isub]=linalg.inv(power_pcov)
+    
+        inv_cov_mean = np.mean(inv_cov_sub,axis=0)
+        #cov_mean = np.zeros((nbin,nbin))
+        #for ik in xrange(0,nbin):
+        #    for jk in xrange(0,nbin):
+        #        cov_mean[ik,jk]=np.mean(cov_sub[:,ik,jk])
+    
+        sigma2 = np.zeros((nbin,nbin))
+        fact = 0.
+        for ik in xrange(0,nbin):
+            for jk in xrange(0,nbin):
+                for isubset in xrange(0,nsub):
+                    sigma2[ik,jk]+=(inv_cov_sub[isubset,ik,jk] - inv_cov_mean[ik,jk]) * (inv_cov_sub[isubset,ik,jk] - inv_cov_mean[ik,jk])
+                sigma2[ik,jk]/=((nr-nbin-2)*inv_cov_mean[ik,ik]*inv_cov_mean[jk,jk]+(nr-nbin)*inv_cov_mean[ik,jk]**2)
+        sigma2/=float(nsub-1)
+    
+        sigma2_all[i]=sigma2
+        
+        i+=1
+    return list_nr, sigma2_all
 # ---------------------------------------------------------------------------- #
 
 